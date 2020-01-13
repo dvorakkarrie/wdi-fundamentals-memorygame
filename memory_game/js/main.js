7fb8135d54e6cc4b029a180c3aea17d281b5a3de
Karrie Dvorak <dvorakkarriel@johndeere.com>
2020-01-12 18:37:33 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,7 @@
-// arrays used for memory card game
-const cards = ['queen','queen','king','king'];
-let cardsInPlay = [];
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
 
-// variables used for card game
-let cardOne = cards[0];
-let cardTwo = cards[2];
-
-// push to add card to cardsInPlay array
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-// Display the card the user flipped.
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-} 
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
