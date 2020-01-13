c33076e7b12fdb722526a29507e33bce6e0243a2
Karrie Dvorak <dvorakkarriel@johndeere.com>
2020-01-13 13:30:10 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 // Define arrays for the cards
-let cards = ['queen','queen','king','king'];
+let cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -7,9 +29,9 @@ function checkForMatch() {
 	// An if statement to check if the length of the cardsInPlay array is 2.
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		} else {
-			console.log("Sorry, try again."); 
+			alert("Sorry, try again."); 
 		}
 	};
 };
@@ -17,8 +39,10 @@ function checkForMatch() {
 function flipCard(cardID) {
 
 	// Display card that was flipped over
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+	cardsInPlay.push(cards[cardID].rank);
 	checkForMatch();
 };
 
