4934bfa482d61393854029ff85ee3fd1f3d35079
Karrie Dvorak <dvorakkarriel@johndeere.com>
2020-01-12 19:35:29 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,24 +2,28 @@
 let cards = ['queen','queen','king','king'];
 let cardsInPlay = [];
 
-// Define variables for the four king and queen cards
-let cardOne = cards[0];
-let cardTwo = cards[2];
-
-// Add the first card to the cardsInPlay array.
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-// Messages displayed in Developer section
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-// An if statement to check if the length of the cardsInPlay array is 2.
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else 
-		alert("Sorry, try again."); 
+function checkForMatch() {
+		
+	// An if statement to check if the length of the cardsInPlay array is 2.
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again."); 
+		}
+	};
 };
+
+function flipCard(cardID) {
+
+	// Display card that was flipped over
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
 
