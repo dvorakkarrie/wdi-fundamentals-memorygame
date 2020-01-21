a4608784404b49cfd2901e54b95430be0a914a2c
Dvorak Karrie L <ks70902@wdxks70902vcg.jdnet.deere.com>
2020-01-21 14:57:52 -0600

    created game logic

--- memory_game_mac/js/main.js
+++ memory_game_mac/js/main.js
@@ -1,20 +1,43 @@
 //Define array for cards and cardsInPlay.
-const cards = ['queen','queen','king','king'];
+const cardsArray = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png",
+},
+{	
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png",
+},
+{	
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png",
+},
+{	
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png",
+}
+];
+
 const cardsInPlay= [];
 
 function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You've found a match!");
+		alert("You've found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 function flipCard(cardId) {
 
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
-
+	cardsInPlay.push(cardsArray[cardId].rank);
+	console.log("User flipped " + cardsArray[cardId].rank);
+	console.log(cardsArray[cardId].suit);
+	console.log(cardsArray[cardId].cardImage);
 
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
