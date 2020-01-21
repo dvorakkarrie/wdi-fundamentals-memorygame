594f5e437dbccdcb5dbef36970108aa9fe41e359
Dvorak Karrie L <ks70902@wdxks70902vcg.jdnet.deere.com>
2020-01-21 13:27:00 -0600

    created game logic

--- memory_game_mac/js/main.js
+++ memory_game_mac/js/main.js
@@ -1,9 +1,23 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+//Define array for cards and cardsInPlay.
+const cards = ['queen','queen','king','king'];
+const cardsInPlay= [];
+
+//Define variables for selected cards.
+let cardOne = cards[0];
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("Up and running!");
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
